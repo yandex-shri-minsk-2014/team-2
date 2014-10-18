c8604cf2d1e20b1fdb943296424df35075bfafd2
Eugene Daragan <eugene.daragan@codex-soft.com>
2014-10-18 16:14:19 +0300

    Added browserify and npm dependecies

--- gulp/config.js
+++ gulp/config.js
@@ -1,15 +1,24 @@
-var dest = 'build'
+var dest = 'build';
 
 module.exports = {
   copyStatic: {
-    src: 'client/**',
-    dst: dest
+    src: 'client/static/**',
+    dst: dest + '/static/'
   },
   css: {
     src: 'blocks/**/*.css',
-    concatSrc: 'index/index.css',
+    concatSrc: 'index.css',
     browsers: ['last 2 versions'],
     cascade: false,
-    dest: dest
+    dest: dest + '/css'
+  },
+  browserify: {
+    debug: true,
+    extensions: ['.js'],
+    bundleConfigs: [{
+      entries: __dirname + '/../client/js/app.js',
+      dest: dest,
+      outputName: 'js/app.min.js'
+    }]
   }
 }
