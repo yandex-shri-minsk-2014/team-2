c67a5392a307f1896ed3edf7b79cdeffb860160f
Eugene Daragan <eugene.daragan@codex-soft.com>
2014-10-18 18:39:15 +0300

    Ранний набросок сокетов на клинте, формирование списка

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
-console.log('Hello world');
+var COLORS = ['#E8716B','#47B39D','#FFAB89','#FDBF53','#70425E'];
+
+var socket = io();
+var $users = $('.users-list');
+
+var users = [{name: 'Вася'}, {name: 'Петя'}];
+
+// socket.on('usersUpdate', function(data) {
+var update = function(data) {
+  $users.empty();
+  data.forEach(function(user, index) {
+    $users.append(
+      $('<li class="user">')
+        .text(user.name)
+        .css('backgroudColor', COLORS[index % COLORS.length])
+    );
+  })
+});
+
+update(users);
