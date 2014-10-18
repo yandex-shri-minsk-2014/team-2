29620866897625e28910cfab4d9f18f7f54a1113
April <egor@77.88.25.23-by.dhcp.yndx.net>
2014-10-18 14:56:56 +0300

    Added watch for css

--- gulp_/config.js
+++ gulp_/config.js
@@ -1,15 +1,11 @@
-var dest = 'build'
+var dest = 'pages/'
 
 module.exports = {
-  copyStatic: {
-    src: 'client/**',
-    dst: dest
-  },
   css: {
     src: 'blocks/**/*.css',
     concatSrc: 'index/index.css',
     browsers: ['last 2 versions'],
     cascade: false,
     dest: dest
-  }
+  },
 }
--- gulp_/tasks/watch.js
+++ gulp_/tasks/watch.js
@@ -2,6 +2,5 @@ var gulp = require('gulp');
 var config = require('../config');
 
 gulp.task('watch', function() {
-  gulp.watch(config.copyStatic.src, ['copyStatic']);
   gulp.watch(config.css.src, ['css']);
-})
+});
