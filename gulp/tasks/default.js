1076f35c267ee945ab6b17ea24980c59093ad63d
Semigradsky <d.semigradsky@itransition.com>
2014-10-20 14:59:22 +0300

    Добавлен jscs

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['jshint', 'watch', 'copyStatic', 'css', 'browserify', 'supervisor']);
+gulp.task('default', ['jshint', 'jscs', 'watch', 'copyStatic', 'css', 'browserify', 'supervisor']);
