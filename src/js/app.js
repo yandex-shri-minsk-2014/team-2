81df236ad25bf0fda4f671c0448823c2c0a0a8af
Egor Lipskiy <elipskij@hotmail.com>
2014-10-25 14:43:23 +0300

    Обновление позиции курсора в статусбаре

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ var connection = require('./socket')();
 var guard = require('./guard')();
 var editor = require('./editor')();
 
+editor.init();
 connection.init();
 getUserName();
 
--- src/js/editor.js
+++ src/js/editor.js
@@ -2,24 +2,31 @@ module.exports = function() {
   'use strict';
 
   var editor = ace.edit('ace-editor');
-  var sbLine = document.getElementById('statusbar__line');
-  var sbColumn = document.getElementById('statusbar__column');
+  var sbPosition = document.getElementById('statusbar__position');
 
-  editor.setTheme('ace/theme/solarized_dark');
+  function init() {
+    editor.setTheme('ace/theme/solarized_dark');
 
-  editor.getSession().setMode('ace/mode/javascript');
-  editor.getSession().setUseSoftTabs(true);
-  editor.getSession().setUseWrapMode(true);
+    editor.getSession().setMode('ace/mode/javascript');
+    editor.getSession().setUseSoftTabs(true);
+    editor.getSession().setUseWrapMode(true);
 
-  editor.setShowPrintMargin(true);
-  editor.setShowInvisibles(true);
+    editor.setShowPrintMargin(true);
+    editor.setShowInvisibles(true);
+  }
 
-  editor.on('change', function(e) {
-    // console.log(e);
-  });
-  editor.getSession().selection.on('changeCursor', function(e) {
-    sbLine.innerHTML = editor.getCursorPosition().row + 1;
-    sbColumn.innerHTML = editor.getCursorPosition().column + 1;
-    console.log(editor.getCursorPosition());
-  })
+  editor.on('change', function(e) {  });
+
+  editor.getSession().selection.on('changeCursor', updateStatusBarPosition)
+
+  function updateStatusBarPosition(data) {
+    var cursorPosition = editor.getCursorPosition();
+
+    sbPosition.innerHTML = 'Line: ' + (cursorPosition.row + 1).toString() +
+     ', Column: ' + (cursorPosition.column + 1).toString();
+  }
+
+  return {
+    init: init
+  }
 };
