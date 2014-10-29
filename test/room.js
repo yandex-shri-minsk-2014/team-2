38b2336ccc8dedd3c9059f0abe36a8158759a332
Semigradsky <d.semigradsky@itransition.com>
2014-10-29 09:49:03 +0300

    Улучшены тесты

--- test/room.js
+++ test/room.js
@@ -4,23 +4,26 @@ var should = require('chai').expect,
     Room = require('../server/room.js');
 
 describe('Room', function() {
+  var fakeColorGenerator,
+      room;
 
-  var fakeColorGenerator = {
-    getColor: function() { return 'black'; },
-    restoreColor: function() { }
-  };
+  beforeEach(function(){
+    fakeColorGenerator = {
+      getColor: function() { return 'black'; },
+      restoreColor: function() { }
+    };
+    room = new Room(fakeColorGenerator);
+  });
 
 
-  describe('addUser', function() {
+  describe('addUser(user)', function() {
 
     it('should add the user', function() {
-      var room = new Room(fakeColorGenerator);
       room.addUser({ userId: 0 });
       should(room.getUsers().length).to.eq(1);
     });
 
     it('should throw an exception if trying to add the user who is already in the room', function() {
-      var room = new Room(fakeColorGenerator);
       should(function() {
         room.addUser({ userId: 0 });
       }).to.not.throw(Error);
@@ -30,26 +33,25 @@ describe('Room', function() {
     });
 
     it('at the user added to the room must be some color', function() {
-      var room = new Room(fakeColorGenerator);
       room.addUser({ userId: 0 });
-      should(false, room.getUsers()[0].userColor).to.exist;
+      should(room.getUsers()[0].userColor).to.exist;
     });
 
   });
 
 
-  describe('removeUser', function() {
+  describe('removeUser(userId)', function() {
+
+    beforeEach(function() {
+      room.addUser({ userId: 0 });
+    });
 
     it('should remove the user', function() {
-      var room = new Room(fakeColorGenerator);
-      room.addUser({ userId: 0 });
       room.removeUser(0);
       should(room.getUsers().length).to.be.empty;
     });
 
     it('should return true/false if the user you want to delete, there is a room/no in room', function() {
-      var room = new Room(fakeColorGenerator);
-      room.addUser({ userId: 0 });
       should(room.removeUser(0)).to.be.true;
       should(room.removeUser(1)).to.be.false;
     });
@@ -57,10 +59,9 @@ describe('Room', function() {
   });
 
 
-  describe('getUsers', function() {
+  describe('getUsers()', function() {
 
     it('change the returned array should not affect the users in the room', function() {
-      var room = new Room(fakeColorGenerator);
       room.addUser({ userId: 0 });
       room.getUsers().push(42);
       should(room.getUsers().length).to.eq(1);
