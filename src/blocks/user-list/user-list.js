bec79e4691bef204d70b7fccb20d4872714f8a6c
semigradsky <semigradskyd@gmail.com>
2014-11-07 23:04:16 +0300

    Рефакторинг

--- src/blocks/user-list/user-list.js
+++ src/blocks/user-list/user-list.js
@@ -1,60 +1,21 @@
-module.exports = function() {
-  'use strict';
+'use strict';
 
-  var $ = require('jquery');
-  var io = require('socket.io-client');
-  var socket = io();
-  var userList;
+var socket = require('../../js/socket');
+var $ = require('jquery');
 
-  function init(editor) {
-    userList = $('.user-list');
+function onUsersUpdate(users) {
+  var userList = $('.user-list');
+  userList.empty();
 
-    socket.on('usersUpdate', onUsersUpdate);
-    socket.on('changeRoom', onChangeRoom);
-    socket.on('markerUpdate', editor.updateCursorMarker);
-    socket.on('markerRemove', editor.removeMarker);
-    socket.on('connect', function() {
-      socket.emit('connectToRoom', window.location.pathname.slice(1));
-    });
-  }
+  users.forEach(function(user) {
+    userList.append(
+      $('<li class="user">')
+        .append(
+          $('<figure class="user__color">').css('backgroundColor', user.userColor)
+        )
+        .append(user.userName)
+    );
+  });
+}
 
-  function verifyUserName(userName, verifyCallback) {
-    socket.emit('verifyUserName', userName);
-    socket.off('verifyUserNameAnswer');
-    socket.on('verifyUserNameAnswer', verifyCallback);
-  }
-
-  function onUsersUpdate(data) {
-    userList.empty();
-
-    data.forEach(function(user) {
-      userList.append(
-        $('<li class="user">')
-          .append(
-            $('<figure class="user__color">').css('backgroundColor', user.userColor)
-          )
-          .append(user.userName)
-        );
-      });
-    }
-
-  function onChangeRoom(data) {
-    window.history.pushState(1, document.title, data.roomId);
-  }
-
-  function connect(userName) {
-    socket.emit('userConnect', userName);
-  }
-
-  function sendMarker(data) {
-    socket.emit('userCursorPosition', data);
-  }
-
-  return {
-    init: init,
-    connect: connect,
-    verifyUserName: verifyUserName,
-    sendMarker: sendMarker,
-    socket: socket.io.engine
-  };
-};
+socket.on('usersUpdate', onUsersUpdate);
