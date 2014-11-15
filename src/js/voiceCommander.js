9d5551b33eaa3e8058ee1fa49f60258e027d1697
SHRI <shri@77.88.25.24-by.dhcp.yndx.net>
2014-11-15 17:56:55 +0300

    Added voice commander

---
+++ src/js/voiceCommander.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var commands = {
+  hello: function() { window.alert('Hello world!'); },
+  'best command': function() { window.alert('Muffin Commandos') }
+};
+
+if (annyang) {
+  annyang.addCommands(commands);
+  annyang.start();
+}
